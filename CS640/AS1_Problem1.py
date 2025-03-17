9f15d464c04bad30e59a38eab45ec339499e32be
Keshav Mishra <157668993+aim4astar@users.noreply.github.com>
2025-03-16 20:25:40 -0500

    Update AS1_Problem1.py

--- CS640/AS1_Problem1.py
+++ CS640/AS1_Problem1.py
@@ -1,18 +1,18 @@
 from collections import deque
 
-def isValid(state):
-    M, C, B = state
-    M_right, C_right = 3 - M, 3 - C
+def is_valid(state):
+    M_left, C_left, B = state
+    M_right, C_right = 3 - M_left, 3 - C_left
 
-    if(M > 0 and M < C) or (M_right > 0 and M_right < C_right):
+    if(M_left > 0 and M_left < C_left) or (M_right > 0 and M_right < C_right):
         return False
     
     return True
 
-def get_next_state(state):
-    M, C, B = state
+def get_next_states(state):
 
-    moves = [(1, 0), (2, 0), (0, 1), (0, 2), (1, 1)]
+    M, C, B = state
+    moves = [(1, 0), (2, 0), (1, 1), (0, 1), (0, 2)]
 
     next_states = []
 
@@ -22,43 +22,42 @@ def get_next_state(state):
         else:
             next_state = (M + m, C + c, 'L')
 
-        if(0 <= next_state[0] <= 3 and 0 <= next_state[1] <= 3 and isValid(next_state)):
+        if(0 <= next_state[0] <= 3 and 0 <= next_state[1] <= 3 and is_valid(next_state)):
             next_states.append(next_state)
-        
-
+    
+    print("State: ", state, "Generated states: ", next_states)
     return next_states
+        
 
 def bfs():
     start = (3, 3, 'L')
     goal = (0, 0, 'R')
-
     queue = deque([(start, [])])
-
     visited = set()
+
 
     while(queue):
         state, path = queue.popleft()
-
         if state in visited:
             continue
 
         visited.add(state)
         new_path = path + [state]
 
-        if(state == goal):
+        if state == goal:
             return new_path
         
-        for next_state in get_next_state(state):
+        for next_state in get_next_states(state):
             queue.append((next_state, new_path))
-
+    
     return None
 
 solution = bfs()
 if solution:
     for step in solution:
-        print(f"Missionaries(Left): {step[0]} Cannibals(Left): {step[1]} Boat: {step[2]}")
+        print("Missionaries: ", step[0], "Cannibals: ", step[1], "Boat: ", step[2])
 
 else:
-    print("Solution not found! :/")
-        
+    print("No solution found")
+    
 
